e275db12749021272b4d48d57b88b4d136668cc6
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2020-06-15 16:02:51 +0200

    Update vSivine.c

--- testExams/vSivine.c
+++ testExams/vSivine.c
@@ -17,23 +17,19 @@ int main(int argc,char** argv){
     fprintf(izhod,"%d %d\n",sirina,visina);
     fprintf(izhod,"255\n");
     
-    
-    for(int i = 0; i<visina; i++){
+     for(int i = 0; i<visina; i++){
         for(int j = 0; j<sirina; j++){
             sum=0;
             R=fgetc(vhod);
             G=fgetc(vhod);
             B=fgetc(vhod);
             sum=(30*R+59*G+11*B)/100;
-        
             fprintf(izhod,"%c",sum);
-            
-		}
-    }
+        }
+     }
     
     fclose(vhod);
     fclose(izhod);
-    
     
     return 0;
 }
